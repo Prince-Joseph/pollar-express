c4efcd535978bf77f0c0b62a6a09db2a5f605034
S E Prince Joseph <sindala.prince@gmail.com>
2023-04-26 01:44:32 +0530

    + poll results functionality
    Co-authored-by: Namratha Shivani  <cactus001@users.noreply.github.com>
    Co-authored-by: S E Prince Joseph <Prince-Joseph@users.noreply.github.com>

--- static/scripts/question.ts
+++ static/scripts/question.ts
@@ -1,7 +1,10 @@
+const pollIdElement = document.querySelector("#pollId") as HTMLInputElement;
 const questionElement = document.querySelector("#question") as HTMLDivElement;
 //@ts-ignore
 const choicesElement = document.querySelector("#choices") as HTMLDivElement;
 
+const pollId = pollIdElement?.value as string;
+console.log(pollId);
 
 function fetchUpdate(apiUrl){
     fetch(apiUrl)
@@ -26,10 +29,17 @@ interface questionData {
     expireAt ?: string,
     timeLeft ?:number,
     count ?: number,
+    voters ?: Array<any>,
+    latest_voters ?: Array<any>,
 }
 
 function updateUI(data: questionData){
-    questionElement.innerHTML = `${data["question"]} ${data["isActive"]} ${data["expireAt"]} <br> ${data["timeLeft"]} secs` ?? "";
+    questionElement.innerHTML = `
+    ${data["question"]} ${data["isActive"]} ${data["expireAt"]}
+    <br> ${data["timeLeft"]} secs <br> ${data["count"]}
+    <br>${data["voters"]}<br>
+    <br>${data["latest_voters"]}<br>
+    ` ?? "";
     
     const choices = data["choices"] as Array<choiceData>;
     choicesElement.innerHTML = ``;
@@ -37,16 +47,23 @@ function updateUI(data: questionData){
     // create new element
     for (const choice of choices){
         const choiceEl = document.createElement("p");
-        choiceEl.innerHTML = `${choice["value"]} ${choice["count"]}` ?? "";
+        choiceEl.innerHTML = `
+        ${choice["value"]} ${choice["count"]}
+        <br>
+        <div class="bar-container">
+          <div style="width:${Number(choice["count"])/Number(data["count"])*100}%;" class="bar"></div>
+        </div>
+        ` ?? "";
         choicesElement.appendChild(choiceEl);
     }
 }
 
 
 var intervalId = window.setInterval(function(){
-    fetchUpdate('/question/1/');
+    fetchUpdate(`/question/${pollId}/`);
   }, 1000);
 
+fetchUpdate(`/question/${pollId}/`);
 
 
 
