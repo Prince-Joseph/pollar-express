b69e577d7cbb7a387894e8f61acfaa9d8054ab5a
S E Prince Joseph <sindala.prince@gmail.com>
2023-04-25 10:14:32 +0530

    + complete user & admin flow; question & choice create form
    Co-authored-by: NeelimaaKottam <NeelimaaKottam@users.noreply.github.com>
    Co-authored-by: S E Prince Joseph <Prince-Joseph@users.noreply.github.com>

--- polls/urls.py
+++ polls/urls.py
@@ -1,19 +1,28 @@
 from django.urls import path
-from .views import (
-    questions_api,
+from .apis import (
     question_api,
+    question_create_api,
     start_question,
     stop_question,
-    question_create_api,
-    vote,
     extend_duration_question,
+    vote,
+)
+
+from .views import (
+    lobby,
+    login,
     submit_poll,
+    profile_update,
+    
+    questions,
+    create_question,
     question_manager,
+    poll_result
 )
 from django.views.generic import TemplateView
 
 api_url_patterns = [
-    path("questions/", questions_api, name="questions_api"),
+    # path("questions/", questions_api, name="questions_api"),
     path("question/<int:poll_id>/", question_api, name="question_api"),
     path("question/create/", question_create_api, name="question_create_api"),
     path("question/start/<int:poll_id>/", start_question, name="start_question"),
@@ -24,12 +33,29 @@ api_url_patterns = [
 ]
 
 template_url_patterns = [
-    path("", TemplateView.as_view(template_name="index.html")),
-    # Admin Views
-    path("poll/<int:poll_id>/", submit_poll, name="poll"),
-    path("question/manage/<int:poll_id>/", question_manager, name="question_manager"),
+    
+    # lobby
+    path("", lobby, name='lobby'),
+    
+    # Login
+    path("login/", login, name='login'),
     
     # User Views
+    # 1. submit vote    /u/
+    path("u/", profile_update, name="profile_update"),
+    # 2. Profile Update /u/vote/<int:poll:id>/
+    path("u/vote/<int:poll_id>", submit_poll, name="submit_poll"),
+    
+    # Admin Views
+    # Questions /polls/
+    path("polls/", questions, name="questions"),
+    # Create Question /poll/create/
+    path("poll/create/", create_question,name="create_question"),
+    # Manage Question /poll/manage/<int:poll_id>/
+    path("question/manage/<int:poll_id>/", question_manager, name="question_manager"),
+    # Poll Results /poll/result/<int:poll_id>/
+    path("poll/result/<int:poll_id>/", poll_result,name="poll_result"),
+
 ]
 
 urlpatterns = template_url_patterns + api_url_patterns
